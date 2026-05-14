0297f9af64ad7fd813e57d27d9dc3c6a6807f6fc
Michael T. Retholtz <retholtz@gmail.com>
2026-05-14 15:37:12 -0400

    Bump application version to 1.33

--- app/src/main/java/com/retholtz/shadowlink/Main.kt
+++ app/src/main/java/com/retholtz/shadowlink/Main.kt
@@ -13,7 +13,7 @@ import javax.swing.event.DocumentListener
 import javax.swing.filechooser.FileNameExtensionFilter
 
 // --- GLOBAL STATE ---
-const val APP_VERSION = "1.32"
+const val APP_VERSION = "1.33"
 const val GITHUB_REPO = "retholtz/ShadowLink"
 
 var profiles = mutableListOf<Profile>()
